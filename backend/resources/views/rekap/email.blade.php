de952a09a5ad3db0a282ee73021bfecb82a8c2d9
Ubaidmasrur <66166577+Ubaidmasrur@users.noreply.github.com>
2025-07-11 20:45:59 +0700

    Create email.blade.php

---
+++ backend/resources/views/rekap/email.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Rekap Nilai</title>
+</head>
+<body>
+    <p>Yth. Wali dari {{ $namaSiswa }},</p>
+    <p>Berikut kami lampirkan rekap nilai tahunan anak Anda dalam bentuk PDF.</p>
+    <p>Terima kasih atas kerjasamanya.</p>
+</body>
+</html>
