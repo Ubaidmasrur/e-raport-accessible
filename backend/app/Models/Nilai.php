3cd61d700d98a5197391db64c3df2eba0254e0d4
Ubaidmasrur <66166577+Ubaidmasrur@users.noreply.github.com>
2025-07-11 20:24:50 +0700

    Update Nilai.php

--- backend/app/Models/Nilai.php
+++ backend/app/Models/Nilai.php
@@ -6,3 +6,4 @@ class Nilai extends Model {
     protected $fillable = ['siswa_id', 'indikator_id', 'nilai', 'semester', 'tahun_ajaran_id'];
     public function siswa() { return $this->belongsTo(Siswa::class); }
     public function indikator() { return $this->belongsTo(Indikator::class); }
+}
