5a705be400869f0882626cae26bcc8731f746a92
Ubaidmasrur <66166577+Ubaidmasrur@users.noreply.github.com>
2025-07-11 20:10:32 +0700

    api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,10 +1,15 @@
 <?php
 use Illuminate\Support\Facades\Route;
-Route::post('/login', 'AuthController@login');
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\NilaiController;
+use App\Http\Controllers\RekapController;
+
+Route::post('/login', [AuthController::class, 'login']);
+
 Route::middleware('auth:sanctum')->group(function () {
- Route::get('/user', 'AuthController@me');
- Route::apiResource('nilai', NilaiController::class);
- Route::apiResource('siswa', SiswaController::class);
- Route::apiResource('indikator', IndikatorController::class);
- Route::get('/rekap/pdf', 'RekapController@generatePDF');
+    Route::get('/user', [AuthController::class, 'me']);
+    Route::apiResource('nilai', NilaiController::class);
+    Route::apiResource('siswa', SiswaController::class);
+    Route::apiResource('indikator', IndikatorController::class);
+    Route::get('/rekap/pdf', [RekapController::class, 'generatePDF']);
 });
