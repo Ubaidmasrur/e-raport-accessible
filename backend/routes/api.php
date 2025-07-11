4cdc53e2700738d2f5dcba2705ebc390f8086265
Ubaidmasrur <66166577+Ubaidmasrur@users.noreply.github.com>
2025-07-11 20:35:35 +0700

    Update api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,10 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\{AuthController, NilaiController, SiswaController, IndikatorController, TahunAjaranController, UserController, RekapController};
+use App\Http\Controllers\{
+    AuthController, NilaiController, SiswaController, 
+    IndikatorController, TahunAjaranController, UserController, RekapController
+};
 
 Route::post('/login', [AuthController::class, 'login']);
 
