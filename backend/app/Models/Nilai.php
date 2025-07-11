f3b83624dcc05bcf9b6aac72c0f828e6aaa3dcfd
Ubaidmasrur <66166577+Ubaidmasrur@users.noreply.github.com>
2025-07-11 20:24:24 +0700

    Update Nilai.php

--- backend/app/Models/Nilai.php
+++ backend/app/Models/Nilai.php
@@ -2,7 +2,7 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
-class Nilai extends Model
-{
-    protected $fillable = ['siswa_id','indikator_id','nilai','semester','tahun_ajaran_id'];
-}
+class Nilai extends Model {
+    protected $fillable = ['siswa_id', 'indikator_id', 'nilai', 'semester', 'tahun_ajaran_id'];
+    public function siswa() { return $this->belongsTo(Siswa::class); }
+    public function indikator() { return $this->belongsTo(Indikator::class); }
