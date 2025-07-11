4187f9098ff237faebdf2aaa25be7178d7e5121f
Ubaidmasrur <66166577+Ubaidmasrur@users.noreply.github.com>
2025-07-11 20:16:50 +0700

    pdf.blade.php

--- backend/resources/views/rekap/pdf.blade.php
+++ backend/resources/views/rekap/pdf.blade.php
@@ -1,1 +1,7 @@
-<html><body><h1>Rekap Nilai</h1></body></html>
+<html>
+<head><title>Rekap Nilai</title></head>
+<body>
+    <h1>Rekap Nilai Tahunan</h1>
+    <p>Nama: {{ $siswa->nama }}</p>
+</body>
+</html>
